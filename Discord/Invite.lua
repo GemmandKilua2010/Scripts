4076d2c4ff2230ed4075f148244e0d82ba46dd0b
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-08 16:24:18 -0300

    Add DiscordRPC module for invite functionality

---
+++ Discord/Invite.lua
@@ -0,0 +1,32 @@
+local HttpService = game:GetService("HttpService")
+
+local DiscordRPC = {}
+local RPC_URL = "http://127.0.0.1:6463/rpc?v=1"
+
+function DiscordRPC:OpenInvite(Code)
+    assert(type(Code) == "string", "ERRO: Code precisa ser uma string")
+
+    local body = {
+        args = {
+            code = Code,
+            sex = "?species=Goblin&realm=Toril"
+        },
+        cmd = "INVITE_BROWSER",
+        nonce = tostring(math.random(100000, 999999999))
+    }
+
+    local success, response = pcall(function()
+        return request({
+            Url = RPC_URL,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["origin"] = "https://ptb.discord.com"
+            },
+            Body = HttpService:JSONEncode(body)
+        })
+    end)
+    return success, response
+end
+
+return DiscordRPC
